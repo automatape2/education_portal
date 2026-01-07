c281d972e2ae90419c9ad42a3faa37f18824284f
Jose Anicama <j.j.anicamatanta@gmail.com>
2026-01-07 17:22:46 -0500

    feat: Make sidebar full height for improved layout consistency

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -8,7 +8,7 @@
             $newMessagesCount = 5;
         @endphp
         
-        <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900 !h-screen">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
             <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
